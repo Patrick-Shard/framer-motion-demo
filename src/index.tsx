352577b95a3e344cca4b545b9d19f6085059d3af
Patrick <45369409+Patrick-Shard@users.noreply.github.com>
2023-03-10 10:42:33 +0100

    🎉 Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ChaynsProvider } from 'chayns-api';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+
+// @ts-expect-error chayns-js api has no types
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+chayns.ready
+    .then(() => {
+        try {
+            createRoot(document.querySelector('#root') as Element)
+                .render(<ChaynsProvider><App /></ChaynsProvider>);
+        } catch (e) {
+            console.error('Encountered error at `ReactDOM.render`: ', e);
+        }
+    })
+    .catch((error: Error) => {
+        console.warn('No chayns environment found.', error);
+    });
